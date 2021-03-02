f040840b1b131348a93f4edf0608a34540a77d60
Sammy Mutahi <sammymutahi12@gmail.com>
2021-03-02 09:49:18 +0300

    Add naviagtion ui

--- app/src/main/java/com/sammy/arcgisapp/MainActivity.kt
+++ app/src/main/java/com/sammy/arcgisapp/MainActivity.kt
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.esri.arcgisruntime.ArcGISRuntimeEnvironment
+import com.esri.arcgisruntime.geometry.Point
+import com.esri.arcgisruntime.geometry.SpatialReferences
 import com.esri.arcgisruntime.mapping.ArcGISMap
 import com.esri.arcgisruntime.mapping.BasemapStyle
 import com.esri.arcgisruntime.mapping.Viewpoint
+import com.esri.arcgisruntime.mapping.view.Graphic
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay
 import com.esri.arcgisruntime.mapping.view.MapView
+import com.esri.arcgisruntime.symbology.SimpleLineSymbol
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol
 import com.sammy.arcgisapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +22,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         setUp()
+        addGraphics()
     }
+
+    private fun addGraphics() {
+        val graphicsOverlay = GraphicsOverlay()
+        binding.mapView.graphicsOverlays.add(graphicsOverlay)
+        // create a point geometry with a location and spatial reference
+        // Point(latitude, longitude, spatial reference)
+        val point = Point(-118.8065, 34.0005, SpatialReferences.getWgs84())
+        // create an opaque orange (0xFFFF5733) point symbol with a blue (0xFF0063FF) outline symbol
+        val simpleMarkerSymbol = SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CIRCLE, -0xa8cd, 10f)
+
+        val blueOutlineSymbol = SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, -0xff9c01, 2f)
+        simpleMarkerSymbol.outline = blueOutlineSymbol
+
+        // create a graphic with the point geometry and symbol
+        val pointGraphic = Graphic(point, simpleMarkerSymbol)
+
+        // add the point graphic to the graphics overlay
+        graphicsOverlay.graphics.add(pointGraphic)
+    }
+
     private fun setUp(){
         //setup api_key
         ArcGISRuntimeEnvironment.setApiKey("AAPKcfa6e3953cf74fe0ac1e6fec3182f7bemTnSis64O4dZee6vuODz_thUx4JU1XmQik7Gxax8Jk0p7YGM3gIFDyvOxgQilGV4")
